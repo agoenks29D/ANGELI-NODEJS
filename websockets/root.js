c9830f8e9e73e29a75402e1f982409b0aeec11b4
agoenks29D <agungmasda29@gmail.com>
2022-05-14 04:36:30 +0700

    online count by session

--- websockets/root.js
+++ websockets/root.js
@@ -1,1 +1,36 @@
-io.of('/').on('connection', (socket) => {});
+io.of('/').on('connection', (socket) => {
+	const auth_session = socket.handshake.auth.session;
+	if (socket.handshake.auth.session !== undefined) {
+		Models.online.findOne({
+			where: {
+				'session-uid': auth_session
+			}
+		}).then(async online => {
+			if (online == null) {
+				Models.online.create({ 'session-uid': auth_session }).then(async () => {
+					var count = await Models.online.count();
+					io.of('/').emit('online_visitor', count);
+				});
+			} else {
+				online.count = online.get('count') + 1;
+				online.save();
+			}
+		});
+	}
+
+	socket.on('disconnect', () => {
+		Models.online.findOne({ where: { 'session-uid': auth_session } }).then(async online => {
+			if (online !== null) {
+				if ((online.get('count') - 1) == 0) {
+					online.destroy().then(async () => {
+						var count = await Models.online.count();
+						io.of('/').emit('online_visitor', count);
+					})
+				} else {
+					online.count = (online.get('count') - 1);
+					await online.save();
+				}
+			}
+		});
+	});
+});
