49b7f8494718df6222787d63ffcca931c7305cad
agoenks29D <agungmasda29@gmail.com>
2022-05-14 05:24:21 +0700

    added model online-time-span

---
+++ models/online-time-span.js
@@ -0,0 +1,35 @@
+module.exports = function(DataTypes) {
+	return {
+		fields: {
+			id: {
+				type: DataTypes.BIGINT.UNSIGNED,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			'session-uid' : {
+				type: DataTypes.UUID,
+				unique: true,
+				allowNull: false
+			},
+			connected: {
+				type: DataTypes.TIME,
+				allowNull: false
+			},
+			disconnected: {
+				type: DataTypes.TIME,
+				allowNull: true
+			},
+			'time-span': {
+				type: DataTypes.TIME,
+				allowNull: true
+			},
+			date: {
+				type: DataTypes.DATEONLY,
+				allowNull: false
+			}
+		},
+		config: {
+			timestamps: false
+		}
+	}
+}
